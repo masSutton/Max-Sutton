6836eca494f2e812f0214042b2631134f2d5a50a
masSutton <wimoried@gmail.com>
2024-09-22 01:31:05 -0300

    slaedm

---
+++ oq.js
@@ -0,0 +1,4 @@
+document.getElementById('showImagesButton').addEventListener('click', function() {
+    const imageContainer = document.getElementById('imageContainer');
+    imageContainer.classList.toggle('hidden'); // Mostra ou esconde as imagens
+});
